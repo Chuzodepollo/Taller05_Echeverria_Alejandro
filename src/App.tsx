6e4069d1620afddcf03433cdc9fe36a0c7a790a9
Alejandro <alejandro.echeverria.vergara@gmail.com>
2024-09-19 11:25:44 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import miImagen from "./assets/img/Goku.png"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={miImagen} alt="Logo de raect" />
+        <h1>Hola Soy Goku</h1>
+        <p>Este es mi segundo proyecto en React</p>
+      </div>
+    </>
+  )
+}
+
+export default App
